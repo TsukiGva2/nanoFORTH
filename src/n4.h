73d8d2ebd6cef3d16c1d350ffc7b7092fadca716
gnii <chochain@yahoo.com>
2024-04-23 11:38:38 -0700

    ~/src/n4.h bump to v2.2; combine NAME+VERSION

--- src/n4.h
+++ src/n4.h
@@ -21,8 +21,7 @@
 #ifndef __SRC_N4_H
 #define __SRC_N4_H
 
-#define APP_NAME          "\nnanoForth "
-#define APP_VERSION       "2.0 "
+#define APP_NAME          "\nnanoForth 2.2"
 #define N4_API_SZ         8       /**< C API function pointer slots */
 #define TRC_LEVEL         0       /**< tracing verbosity level      */
 
