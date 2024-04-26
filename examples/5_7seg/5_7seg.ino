94616d1370167d5ec21139c4dc47bd219c2fe424
gnii <chochain@yahoo.com>
2024-04-26 15:06:38 -0700

    ~/examples fix 5_7_seg (pin rearranged)

--- examples/5_7seg/5_7seg.ino
+++ examples/5_7seg/5_7seg.ino
@@ -3,8 +3,17 @@
  *  @brief nanoFORTH example - 4 digit 7-segment display
  *
  *  Assuming you have a 12-pin 4 digit 7-segment LED hooked up as following
+ *
+ *      A             D4 A  F  D3 D2 B
+ *     ---       +----^--^--^--^--^--^---+   
+ *  F |   | B    |     |     |     |     |
+ *     -G-       |  8  |  8  |  8  |  8  |
+ *  E |   | C    |     |     |     |     |
+ *     ---       +----v--v--v--v--v--v---+
+ *      D  *DP        E  D  DP C  G  D1
+ *
  *  + A,B,C,D,E,F,G,DP => 4,5,6,7,8,9,10,11
- *  + D1,D2,D3,D4      => 2,3,12,13
+ *  + D1,D2,D3,D4      => A0,A1,A2,A3       i.e. digital pin 14,15,16,17
  *  + or check this Wokwi project https://wokwi.com/projects/358961171560345601
  *
  *  How To:
@@ -18,23 +27,24 @@
 #include <nanoFORTH.h>
 
 const char code[] PROGMEM =
-": ini 12 FOR 1 I 1 + PIN NXT ; ini FGT ini\n"         // set pin 2~13 to OUTPUT
-"VAR x 8 ALO\n"                                        // x keeps 7-seg pin patterns of 0~9
-"$F360 x ! $B5F4 x 2 + ! $66D6 x 4 + !\n"
-"$D770 x 6 + ! $F776 x 8 + !\n"
-"VAR d 2 ALO $3834 d ! $2C1C d 2 + !\n"                // d keeps digit control pin patterns
-": 7d d + C@ DUP $10C OUT $230 OUT ;\n"                // ( n -- ) set output digit
-"VAR vx 2 ALO\n"                                       // vx cache pattern for 4 digits before display
-": vx! 1 - vx + C! ;\n"                                // ( i -- ) update vx by index
-": ?v 4 FOR DUP 10 MOD x + C@ I vx! 10 / NXT DRP ;\n"  // ( n -- ) process number into 4-digit bit patterns
-": 7s vx + C@ DUP $1F0 OUT $20F OUT ;\n"               // ( i -- ) display i'th digit on 7-seg
-"VAR i 0 i !\n"                                        // i is the 7-seg digit to display
-": i++ i @ 1 + 3 AND DUP i ! ;\n"                      // ( -- ) increment i and keep between 0~3
-": dsp i++ DUP 7s 7d ;\n"                              // ( -- ) display 
-"5 0 TMI dsp\n"                                        // timer interrupt to update display every 5ms
-"VAR cnt 0 cnt !\n"                                    // cnt is a counter we want it value on display
-": c++ cnt @ 1 + DUP cnt ! ?v ;\n"                     // ( -- ) increment the counter and cache patterns
-"1000 1 TMI c++\n";                                    // timer interrupt to update c every second
+// "1 TRC\n"                                           // turn tracing on for debugging
+": ini 16 FOR 1 I 1 + PIN NXT ; ini FGT ini\n"         // set pin 2~17 to OUTPUT
+"CRE x $F360 , $B5F4 , $66D6 , $D770 , $F776 ,\n"      // x keeps 7-seg pin patterns of 0~9, 1-byte each
+"CRE vx 4 ALO\n"                                       // vx cache pin patterns for 4 digits
+": vx! 1 - vx + C! ;\n"                                // ( n i -- ) vx[i] = (char)n
+": v! 4 FOR DUP 10 MOD x + C@ I vx! 10 / NXT DRP ;\n"  // ( n -- ) n => pin patterns digit-by-digit
+"CRE d $0E0D , $0B07 ,\n"                              // d keeps 4 digit control (aaaA, aaAa, aAaa, Aaaa)
+": sd d + C@ $30F OUT ;\n"                             // ( i -- ) select output digit to port C (A0~A6)
+"VAR i 0 i !\n"                                        // i controls the i'th digit to display
+": 7s DUP sd vx + C@ DUP $1F0 OUT $20F OUT ;\n"        // ( i -- ) send vx[i] to port D (0~7) & B (8~13)
+": dsp i @ 1 + 3 AND DUP i ! 7s ;\n"                   // ( -- ) cycle i to display the i'th digit
+// "1234 v!\n"                                         // incremental test number
+// "0 dsp 500 DLY 1 dsp 500 DLY 2 dsp 500 DLY 3 dsp\n" // tests 4-digit display
+"5 0 TMI dsp\n"                                        // timer interrupt[0] to update display every 5ms
+"VAR cnt 1000 cnt !\n"                                 // cnt is a counter we want it value on display
+": c++ cnt @ 1 + DUP cnt ! v! ;\n"                     // ( -- ) increment the counter and cache patterns
+"100 1 TMI c++\n"                                      // timer interrupt[1] to update c every 100ms
+;
 
 void setup() {
     Serial.begin(115200);            ///< init Serial Monitor
